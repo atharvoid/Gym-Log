510b301a97c5ba4f23580b07ddebf5df8c43da3c
atharvoid <atharvapatil.connect@gmail.com>
2026-07-19 22:55:22 +0530

    fix(android): enable Java 8 desugaring in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,6 +26,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -109,4 +110,8 @@ if (!hasSentryToken) {
             enabled = false
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
